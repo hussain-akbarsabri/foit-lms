02e381478194985d744f7da2658ec1858a6549e4
Akbar Hussain Sabri <akbar.sabri54@gmail.com>
2021-07-27 12:38:36 +0500

    Initial commit

---
+++ home.php
@@ -0,0 +1,72 @@
+<!DOCTYPE html>
+<html>
+	<head>
+		<title>FoIT LMS</title>
+		<link rel = "icon" type = "png" href = "foitlmsLogo.png">
+		<meta charset="UTF-8">
+		<meta name="viewport" content="width=device-width, initial-scale=1">
+	  	<link rel='stylesheet prefetch' href='http:////netdna.bootstrapcdn.com/font-awesome/4.0.3/css/font-awesome.css'>
+	    <link rel="stylesheet" href="css/style.css">
+		<link rel="stylesheet" href="css/bootstrap.min.css" >
+		<script src="js/jquery.min.js"></script>
+	    <script src="js/bootstrap.min.js"></script>
+	</head>
+	<body>
+
+		<!-- Header -->
+		<?php include("includes/header.php"); ?>
+
+
+		<div class="container">
+			<div class="col-md-3">
+				<br>
+				<ul class="list-group" style="list-style: none;">
+					<li class="list-group-item-danger" style="font-size: 28px; color: black; padding-left: 15px;"> Clock</a> </li>
+					<li class="list-group-item" style="font-size: 16px;"> Server: &nbsp &nbsp &nbsp &nbsp 7:13:56am</li>
+					<li class="list-group-item" style="font-size: 16px;"> You: &nbsp &nbsp &nbsp &nbsp 7:13:56am</li>
+				</ul>
+				<ul class="list-group" style="list-style: none;">
+					<li class="list-group-item-danger" style="font-size: 28px; color: black; padding-left: 15px;"> Navigation</a> </li>
+					<li class="list-group-item" style="font-size: 16px;"> My home </li>
+					<li class="list-group-item" style="font-size: 16px;"> Site home </li>
+					<li class="list-group-item" style="font-size: 16px;"> Site pages </li>
+					<li class="list-group-item" style="font-size: 16px;"> My profile </li>
+					<li class="list-group-item" style="font-size: 16px;"> My courses </li>
+					<br>
+				<ul class="list-group" style="list-style: none;">
+					<li class="list-group-item-danger" style="font-size: 28px; color: black; padding-left: 15px;"> Administration</a> </li>
+					<li class="list-group-item" style="font-size: 16px;"> My profile settings	 </li>
+				</ul>
+			</div>
+			<div class="col-md-9">
+				<br>
+				<h4>If you DO NOT have a username for FoIT-LMS, send an email at foitlms@ucp.edu.pk immediately</h4>
+				<br>
+				<h4>Students can see the following vedios:</h4>
+				<ul>
+					<li>How to attempt a Quiz</li>
+					<li>How to submit an assignment on LMS</li>
+				</ul>
+				<br>
+				<br>
+				<h4>Teachers can see the following vedios:</h4>
+				<ul>
+					<li>Creating Question Bank and a Quiz</li>
+					<li>Question Bank Import Export</li>
+				</ul>
+				<h4>Following documents are available for further help:</h4>
+				<ul>
+					<li>How to create an assignment on LMS</li>
+					<li>How to download own quiz result</li>
+				</ul>
+				<br>
+				<br>
+				<b>In case of any issue, please email us at: foitlms@ucp.edu.pk</b>
+			</div>
+		</div>
+
+		<!-- Footer -->
+		<?php include("includes/header.php"); ?>
+
+	</body>	
+</html>
---
+++ includes/footer.php
@@ -0,0 +1,46 @@
+<div class="well well-md" style="background-color: #a64437;">
+	<div class="container-fluid">
+		<div class="row" style="background-color: #a64437;">
+			
+			<!-- 1 column -->
+			<div class="col-md-3">
+				<img src="img/logofoit.png" class="img img-responsive">
+				<br>
+				<p style="font-size: 17px; color: white;">This system combines normal day to day classroom teaching with real world computer 
+				mediated/assisted learning to foster an effictive teaching and learning process.</p>
+			</div>
+
+			<!-- 2 column -->
+			<div class="col-md-3"	>
+				<h3 style="color: white;">Links</h3>
+				<ul class="list-group" style="list-style: none;">
+					<li style="color: white;">Help</li>
+					<br>
+					<li style="color: white;">Contact Us</li>
+					<br>
+					<li style="color: white;">Our team</li>
+					<br>
+					<li style="color: white;">Logout</li>
+				</ul>
+			</div>
+
+			<!-- 3 column -->
+			<div class="col-md-3">
+				<h3 style="color: white;">Like us on</h3>
+				<h4 style="color: white;"><spam class="badge" style="font-size:26px;">f</spam> Facebook</h4>
+			</div>
+
+			<!-- 4 column -->
+			<div class="col-md-3">
+				<h3 style="color: white;">Contact</h3>
+				<h4 style="color: white;">F003, Ground Floor, UCP, Lahore</h4>
+				<h4 style="color: white;">Phone: +92-42-35880007 (Ext.669)</h4>
+				<h4 style="color: white;">E-mail: info@ucp.edu.pk</h4>
+			</div>
+		</div>
+	</div>
+</div>
+
+<div class="well well-lg" style="margin-top:-23px; border-radius:0px;background-color: #561209;">
+	<p style="color: white; text-align: center;">Copyright @ 2017 - Developed and Powered by <b>Center for Learning Design Team</b></p>
+</div>
