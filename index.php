dbd9bfc81be0d0c088f2b74b00c1f0680968c4af
dev <hussain.akbarsabri@gmail.com>
2022-09-24 21:49:31 +0500

    removed broken links from application

--- index.php
+++ index.php
@@ -6,8 +6,6 @@
 	<link rel="icon" type="png" href="img/foitlmsLogo.png">
 	<meta charset="UTF-8">
 	<meta name="viewport" content="width=device-width, initial-scale=1">
-	<link rel='stylesheet prefetch' href='http:////netdna.bootstrapcdn.com/font-awesome/4.0.3/css/font-awesome.css'>
-	<link rel="stylesheet" href="css/style.css">
 	<link rel="stylesheet" href="css/bootstrap.min.css">
 	<script src="js/jquery.min.js"></script>
 	<script src="js/bootstrap.min.js"></script>
